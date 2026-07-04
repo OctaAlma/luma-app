a7f85a723a5aba05d04afa338a847cecb45ece49
Octavio Almanza <octaalma18@gmail.com>
2026-07-04 14:54:59 -0500

    created scripts to merge non-html files between branches

---
+++ resources/merge-espanol-to-main.ps1
@@ -0,0 +1,23 @@
+$Title    = "Confirm Action"
+$Question = "Are you to merge non-HTML changes from branch español to main?"
+$Choices  = "&Y", "&N"
+
+$Decision = $Host.UI.PromptForChoice($Title, $Question, $Choices, 1)
+
+if ($Decision -eq 0){
+    git switch main
+    git merge español --no-commit --no-ff
+
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host "Merge paused (conflicts may exist, which is normal). Proceeding to restore HTML files..." -ForegroundColor Yellow
+    }
+
+    git checkout -- "*html"
+    git add -A
+    git commit -m "Merged non-HTML files from español"
+    Write-Host "Merge completed" -ForegroundColor Green
+}
+else {
+    Write-Host "Merge from branch español to main was not performed" -ForegroundColor Red
+    Exit
+}
